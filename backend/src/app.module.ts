ad27f16e7f919bf3d443630e133547a51f6a2f4b
Duclot William <william.duclot@gmail.com>
2019-07-21 16:57:16 +0100

    Add empty Admin module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { AdminModule } from './admin/admin.module';
+
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
     UserModule,
     AuthModule,
+    AdminModule,
   ],
   controllers: [AppController],
   providers: [AppService],
