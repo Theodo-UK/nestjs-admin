67bb2f645e922e37684c3e6f0ffb156ed38a417d
Duclot William <william.duclot@gmail.com>
2019-08-01 19:16:24 +0100

    Remove non-useful test

--- src/user/interfaces/user.dto.ts
+++ src/user/interfaces/user.dto.ts
@@ -1,6 +1,7 @@
 export class UserDto {
-  readonly name: string;
-  readonly email: string;
-  readonly password: string;
-  readonly roles: string[];
+  readonly name: string
+  readonly email: string
+  readonly password: string
+  readonly roles: string[]
+  readonly agency: object | null
 }
