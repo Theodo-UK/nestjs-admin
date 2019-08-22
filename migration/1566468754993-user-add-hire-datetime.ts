0089129537c3e46f7a6264152a05a9c03572324d
Sam Bunce <samb@theodo.co.uk>
2019-08-22 11:49:51 +0100

    Add datetime widget

---
+++ migration/1566468754993-user-add-hire-datetime.ts
@@ -0,0 +1,13 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class userAddHireDatetime1566468754993 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query(`ALTER TABLE "users" ADD "hireDatetime" TIMESTAMP`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "hireDatetime"`);
+    }
+
+}
