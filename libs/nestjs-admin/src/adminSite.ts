19fc6589181d9535181e9fb34ce33c1d4e608d84
Duclot William <william.duclot@gmail.com>
2019-08-28 17:19:02 +0100

    Doc: clearer debt comment

--- libs/nestjs-admin/src/adminSite.ts
+++ libs/nestjs-admin/src/adminSite.ts
@@ -66,9 +66,7 @@ class DefaultAdminSite {
   }
 
   async cleanValues(values: { [k: string]: any }, metadata: EntityMetadata) {
-    // @debt architecture "williamd: this should probably be moved to a Form class"
-    // Also, this whole function is a mess of state and special cases. We need to find
-    // a better way to organise this.
+    // @debt architecture "williamd: this should be broken down in `clean` methods of separate widget classes"
     const propertyNames = Object.keys(values)
     const cleanedValues: typeof values = {}
 
