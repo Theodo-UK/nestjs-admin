fd39dbfcdee193bcd829857ba0642cc686adeeec
Duclot William <william.duclot@gmail.com>
2019-08-28 20:13:47 +0100

    App: replace boilerplate with stock nestjs project

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from '@nestjs/common'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return 'Hey you! You probably want to head to <a href="/admin">/admin</a>'
   }
 }
