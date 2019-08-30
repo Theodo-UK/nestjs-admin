863ad46c1304d62f4eb9a816043165991dc97f32
Duclot William <william.duclot@gmail.com>
2019-08-30 10:19:33 +0100

    AdminUserService: export from lib

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common'
 import { AdminUserEntity, DefaultAdminModule } from '@app/nestjs-admin'
 import { AdminSite } from './admin'
 import { AdminController } from './admin.controller'
-import { AdminUserService } from '@app/nestjs-admin/adminUser.service'
+import { AdminUserService } from '@app/nestjs-admin'
 
 @Module({
   imports: [DefaultAdminModule],
