4e215ecbbb99b6a27fa5ac3cd634a804abf68e7a
Sam Bunce <samb@theodo.co.uk>
2019-08-30 12:45:39 +0100

    Return 401 when using POST without authentication

--- libs/nestjs-admin/src/admin.guard.ts
+++ libs/nestjs-admin/src/admin.guard.ts
@@ -1,4 +1,10 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common'
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common'
 import { Reflector } from '@nestjs/core'
 import { Observable } from 'rxjs'
 import UnauthenticatedException from './unauthenticated.exception'
@@ -9,10 +15,17 @@ export class AdminGuard implements CanActivate {
 
   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
     const isPublic = this.reflector.get<boolean>('public', context.getHandler())
+    const request = context.switchToHttp().getRequest()
+
     if (isPublic) {
       return true
     } else {
-      throw new UnauthenticatedException()
+      switch (request.method) {
+        case 'GET':
+          throw new UnauthenticatedException()
+        case 'POST':
+          throw new HttpException('Unauthorized', HttpStatus.UNAUTHORIZED)
+      }
     }
   }
 }
--- test/app.int.ts
+++ test/app.int.ts
@@ -6,7 +6,6 @@ import { AppModule } from '../src/app.module'
 import { Repository } from 'typeorm'
 import { User } from '../src/user/user.entity'
 import { createTestUser } from './utils'
-import { async } from 'rxjs/internal/scheduler/async'
 
 describe('AppController', () => {
   let app: INestApplication
@@ -45,11 +44,16 @@ describe('AppController', () => {
     expect(await userRepository.findOne(user.id)).toBeUndefined()
   })
 
-  it('redirects to login when unauthenticated', async () => {
-    // delete the user via the api call
+  it('redirects to login when unauthenticated using GET', async () => {
     const server = app.getHttpServer()
     const req = await request(server).get(`/admin/user/user`)
     expect(req.status).toBe(302)
     expect(req.header.location).toBe(`/admin/login`)
+  })
+
+  it('returns 403 when unauthenticated using POST', async () => {
+    const server = app.getHttpServer()
+    const req = await request(server).post(`/admin/user/user/add`)
+    expect(req.status).toBe(401)
   })
 })
