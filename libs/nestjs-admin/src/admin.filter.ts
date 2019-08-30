6bb386e1739c1570819a78bb19a7223ee41befa9
Sam Bunce <samb@theodo.co.uk>
2019-08-30 11:21:55 +0100

    Cleaner login redirect

--- libs/nestjs-admin/src/admin.filter.ts
+++ libs/nestjs-admin/src/admin.filter.ts
@@ -1,17 +1,11 @@
 import { ArgumentsHost, Catch, ExceptionFilter, HttpStatus } from '@nestjs/common'
 import UnauthenticatedException from './unauthenticated.exception'
+import { adminUrl } from './admin.filters'
 
 @Catch(UnauthenticatedException)
 export class AdminFilter implements ExceptionFilter {
   public catch(exception: any, host: ArgumentsHost) {
     const res = host.switchToHttp().getResponse()
-    try {
-      res.redirect('/admin/login')
-    } catch (e) {
-      return res.status(500).json({
-        status: HttpStatus.INTERNAL_SERVER_ERROR,
-        message: e.message,
-      })
-    }
+    res.redirect(adminUrl('login'))
   }
 }
