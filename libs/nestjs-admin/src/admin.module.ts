31ed68940906f330e752056cf4536668ca31de3e
Duclot William <william.duclot@gmail.com>
2019-08-31 16:51:19 +0100

    Expose a DefaultAdminModule and an AdminModuleFactory for better devX

--- libs/nestjs-admin/src/admin.module.ts
+++ libs/nestjs-admin/src/admin.module.ts
@@ -2,10 +2,34 @@ import { Module } from '@nestjs/common'
 import { DefaultAdminController } from './admin.controller'
 import DefaultAdminSite from './adminSite'
 import DefaultAdminNunjucksEnvironment from './admin.environment'
+import { injectionTokens } from './tokens'
 
-@Module({
-  controllers: [DefaultAdminController],
-  providers: [DefaultAdminSite, DefaultAdminNunjucksEnvironment],
-  exports: [DefaultAdminSite, DefaultAdminNunjucksEnvironment],
-})
-export class DefaultAdminModule {}
+@Module({})
+export class AdminModuleFactory {
+  static createAdminModule({
+    adminSite = DefaultAdminSite,
+    adminController = DefaultAdminController,
+    adminEnvironment = DefaultAdminNunjucksEnvironment,
+  }) {
+    const adminSiteProvider = {
+      provide: injectionTokens.ADMIN_SITE,
+      useExisting: adminSite,
+    }
+    const adminEnvironmentProvider = {
+      provide: injectionTokens.ADMIN_ENVIRONMENT,
+      useExisting: adminEnvironment,
+    }
+
+    return {
+      module: AdminModuleFactory,
+      controllers: [adminController],
+      // We export the adminSiteProvider, so that the admin site can be injected by the ADMIN_SITE token,
+      // but also the adminSite itself so that the developer can use automatic DI by class.
+      // Same for the adminEnvironment.
+      providers: [adminEnvironment, adminEnvironmentProvider, adminSite, adminSiteProvider],
+      exports: [adminEnvironment, adminEnvironmentProvider, adminSite, adminSiteProvider],
+    }
+  }
+}
+
+export const DefaultAdminModule = AdminModuleFactory.createAdminModule({})
--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,13 +1,3 @@
-import { Controller, Inject } from '@nestjs/common'
 import { DefaultAdminController } from '@app/nestjs-admin'
-import { AdminSite } from './admin'
 
-export class AdminController extends DefaultAdminController {
-  @Inject(AdminSite)
-  // tslint:disable-next-line
-  private _adminSite: AdminSite
-
-  get adminSite() {
-    return this._adminSite
-  }
-}
+export class AdminController extends DefaultAdminController {}
