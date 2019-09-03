0a17e78e2ab56f6eb130860f9a71b365fe24893e
Mike  Riddelsdell <miker@theodo.co.uk>
2019-09-03 12:02:01 +0100

    tslinting

--- libs/nestjs-admin/src/adminUser.service.ts
+++ libs/nestjs-admin/src/adminUser.service.ts
@@ -65,10 +65,11 @@ export class AdminUserService implements EntitySubscriberInterface<AdminUser> {
     return await this.adminUserRepository.findOne({ where: { email } })
   }
 
-  async validateCredentials(email: string, password: string) {
+  async validateCredentials(email: string, pass: string) {
     const adminUser = await this.findOne(email)
-    if (adminUser && this.comparePassword(adminUser, password)) {
-      // @debt quality "miker: is this destructure necessary? was copied from blog post https://dev.to/nestjs/authentication-and-sessions-for-mvc-apps-with-nestjs-55a4"
+    if (adminUser && this.comparePassword(adminUser, pass)) {
+      // @debt quality "miker: 1/ is this destructure necessary? was copied from blog post
+      // @debt quality "miker: 2/ https://dev.to/nestjs/authentication-and-sessions-for-mvc-apps-with-nestjs-55a4"
       const { password, ...result } = adminUser
       return result
     }
