810e9f35a7d3412069cf19722201e14b17da4d25
Duclot William <william.duclot@gmail.com>
2019-09-03 12:21:04 +0100

    Auth: set user in request on login

--- libs/nestjs-admin/src/local.strategy.ts
+++ libs/nestjs-admin/src/local.strategy.ts
@@ -14,6 +14,6 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     if (!adminUser) {
       throw new UnauthorizedException()
     }
-    return true
+    return adminUser
   }
 }
