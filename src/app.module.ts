7c3c00234d208efa2b4472ba301dd36e2777aca0
Duclot William <william.duclot@gmail.com>
2019-09-04 14:47:52 +0100

    Remove unused app auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { AuthModule } from './auth/auth.module'
 import { UserModule } from './user/user.module'
 import { AdminModule } from './admin/admin.module'
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), AdminModule, UserModule, AuthModule],
+  imports: [TypeOrmModule.forRoot(), AdminModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
