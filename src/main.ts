89ad21ee27184c6e38e927b864f854bfa0a5c1ce
Duclot William <william.duclot@gmail.com>
2019-09-09 16:41:31 +0100

    Expose a configureAdminApp function to setup the express app

--- src/main.ts
+++ src/main.ts
@@ -3,63 +3,20 @@ dotenv.config()
 
 import { NestFactory } from '@nestjs/core'
 import { NestExpressApplication } from '@nestjs/platform-express'
-import * as sassMiddleware from 'node-sass-middleware'
-import { join } from 'path'
+import { configureAdminApp } from 'nestjs-admin'
 import { AppModule } from './app.module'
 import { EntityNotFoundFilter } from './exception/entity-not-found.filter'
 import { QueryFailedFilter } from './exception/query-failed.filter'
-import { isDevEnvironment } from './utils/environment'
 
 import * as pgConnect from 'connect-pg-simple'
 import * as session from 'express-session'
-import * as passport from 'passport'
-
-const publicFolder = join(__dirname, '..', 'public')
-const assetPrefix = '/static'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
 
-  app.use(
-    sassMiddleware({
-      src: publicFolder + '/scss',
-      dest: publicFolder + '/css',
-      prefix: assetPrefix + '/css',
-      outputStyle: isDevEnvironment() ? 'expanded' : 'compressed',
-      sourceMap: isDevEnvironment(),
-      debug: isDevEnvironment(),
-    }),
-  )
-
   const PgSession = pgConnect(session)
-  app.use(
-    session({
-      /* @debt TODO "make `store` a configuration of the AdminModule, default on MemoryStore" */
-      store:
-        process.env.SESSION_DB_URL !== 'false' // TODO make this env var actually optional
-          ? new PgSession({ conString: process.env.SESSION_DB_URL })
-          : undefined,
-      secret: 'secret',
-      resave: false,
-      saveUninitialized: false,
-    }),
-  )
-
-  app.use(passport.initialize())
-  app.use(passport.session())
-
-  // needs to be after the sassMiddleware
-  app.useStaticAssets(publicFolder, { prefix: assetPrefix })
-
-  // @debt architecture "miker: should be done by session.serializer.ts in lib"
-  passport.serializeUser(function(user: any, done: (err: Error, user: any) => void): any {
-    done(null, user)
-  })
-  passport.deserializeUser(function(
-    payload: any,
-    done: (err: Error, payload: string) => void,
-  ): any {
-    done(null, payload)
+  configureAdminApp(app, {
+    session: { store: new PgSession({ conString: process.env.SESSION_DB_URL }) },
   })
 
   app.useGlobalFilters(new EntityNotFoundFilter())
