9576e492863e6ea989c5fde5bbc597e306801be4
Duclot William <william.duclot@gmail.com>
2019-09-09 11:52:56 +0100

    CircleCI: test that nestjs-admin can be installed as lib

---
+++ .circleci/templates/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { UserModule } from './user/user.module'
+
+@Module({
+  imports: [TypeOrmModule.forRoot(), UserModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
