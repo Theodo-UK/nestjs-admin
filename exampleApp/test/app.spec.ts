c8341796236edda75f42b3c064449d2dc40a0296
Duclot William <william.duclot@gmail.com>
2019-09-12 18:40:08 +0100

    Test the login

--- exampleApp/test/app.spec.ts
+++ exampleApp/test/app.spec.ts
@@ -3,17 +3,23 @@ import { INestApplication } from '@nestjs/common'
 import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 import * as request from 'supertest'
-import { DefaultAdminModule } from 'nestjs-admin'
+import { DefaultAdminModule, AdminCoreModuleFactory } from 'nestjs-admin'
 import { User } from '../src/user/user.entity'
 import { createTestUser } from './utils'
 import { UserModule } from '../src/user/user.module'
+import { TestAuthModule } from './testAuth/testAuth.module'
 
 describe('AppController', () => {
   let app: INestApplication
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [TypeOrmModule.forRoot(), UserModule, DefaultAdminModule],
+      imports: [
+        TypeOrmModule.forRoot(),
+        UserModule,
+        TestAuthModule,
+        AdminCoreModuleFactory.createAdminCoreModule({}),
+      ],
     }).compile()
 
     app = module.createNestApplication()
@@ -28,8 +34,7 @@ describe('AppController', () => {
     expect(app).toBeDefined()
   })
 
-  // @debt TODO "samb: this should be re-enabled with authentication once we have it"
-  it.skip('can delete a user', async () => {
+  it('can delete a user', async () => {
     // add the user to the database
     const userData = createTestUser({ firstName: 'Max' })
     const userRepository: Repository<User> = app.get(getRepositoryToken(User))
@@ -43,18 +48,5 @@ describe('AppController', () => {
     expect(req.header.location).toBe(`/admin/user/user`)
 
     expect(await userRepository.findOne(user.id)).toBeUndefined()
-  })
-
-  it('redirects to login when unauthenticated using GET', async () => {
-    const server = app.getHttpServer()
-    const req = await request(server).get(`/admin/user/user`)
-    expect(req.status).toBe(302)
-    expect(req.header.location).toBe(`/admin/login`)
-  })
-
-  it('returns 302 when unauthenticated using POST', async () => {
-    const server = app.getHttpServer()
-    const req = await request(server).post(`/admin/user/user/add`)
-    expect(req.status).toBe(302)
   })
 })
---
+++ exampleApp/test/testAuth/testAuth.module.ts
@@ -0,0 +1,13 @@
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common'
+
+@Module({})
+export class TestAuthModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply((req: any, res: any, next: any) => {
+        req.isAuthenticated = () => true
+        next()
+      })
+      .forRoutes('/admin/?')
+  }
+}
