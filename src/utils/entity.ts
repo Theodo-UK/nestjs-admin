4c38e77e96c7be4ecabe96b116f792002fa4da31
Duclot William <william.duclot@gmail.com>
2019-09-13 18:07:56 +0100

    Handle composite primary keys

--- src/utils/entity.ts
+++ src/utils/entity.ts
@@ -2,16 +2,9 @@ import { EntityMetadata } from 'typeorm'
 import { EntityType } from '../types'
 
 export function getPrimaryKeyValue(metadata: EntityMetadata, entity: object) {
-  if (metadata.primaryColumns.length !== 1) {
-    // @debt TODO "williamd: probably still covers most cases, but still TODO"
-    throw new Error(
-      `Entities with composite primary keys unsupported (${
-        metadata.name
-      }). If you have this use case, please open a GitHub issue`,
-    )
-  }
-  return metadata.primaryColumns[0].getEntityValue(entity)
+  return metadata.getEntityIdMixedMap(entity)
 }
+
 export function isEntityInList(entity: EntityType, array: EntityType[], metadata: EntityMetadata) {
   if (!array || !entity) return false
 
