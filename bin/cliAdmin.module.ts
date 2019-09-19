6945b74c6757a7ee4b3ff1d1c9d020591e9f440a
Duclot William <william.duclot@gmail.com>
2019-09-19 11:29:00 +0100

    CLI: better exception when ormconfig not found

--- bin/cliAdmin.module.ts
+++ bin/cliAdmin.module.ts
@@ -4,12 +4,28 @@ import { getConnectionOptions } from 'typeorm'
 import AdminUser from '../src/adminUser.entity'
 import { AdminUserService } from '../src/adminUser.service'
 
+class AdminConnectionException extends Error {
+  constructor(msg) {
+    super(msg)
+  }
+}
+
 @Module({
   providers: [AdminUserService], // @debt TODO "Import DefaultAdminAuthModule instead"
 })
 export class CliAdminModule {
+  static async getConnectionOptions() {
+    try {
+      return await getConnectionOptions()
+    } catch (e) {
+      throw new AdminConnectionException(
+        `Could not connect to your database to create an admin user. Make sure you have an ormconfig file with a default connection or that your environment variables are set (see https://github.com/typeorm/typeorm/blob/master/docs/using-ormconfig.md).`,
+      )
+    }
+  }
+
   static async create() {
-    const connectionOptions = await getConnectionOptions()
+    const connectionOptions = await this.getConnectionOptions()
     const entities = connectionOptions.entities || []
 
     return {
