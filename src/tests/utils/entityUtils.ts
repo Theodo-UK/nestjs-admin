e6175136ceb73f306cd038787ca7f4b780f1ab59
Duclot William <william.duclot@gmail.com>
2019-10-03 18:28:44 +0100

    Tests: use faker to create random data

--- src/tests/utils/entityUtils.ts
+++ src/tests/utils/entityUtils.ts
@@ -1,11 +1,12 @@
+import * as faker from 'faker'
 import { User } from '../../../exampleApp/src/user/user.entity'
 import AdminUser from '../../adminUser.entity'
 
 export function createTestAdminUser(attrs: Partial<AdminUser> = {}): AdminUser {
   const user = new AdminUser()
   const defaultAttrs = {
-    email: 'admin@email.com',
-    password: 'adminpassword',
+    email: faker.internet.email(),
+    password: faker.internet.password(),
   }
   return Object.assign(user, { ...defaultAttrs, ...attrs })
 }
@@ -13,9 +14,9 @@ export function createTestAdminUser(attrs: Partial<AdminUser> = {}): AdminUser {
 export function createTestUser(attrs?: Partial<User>): User {
   const user = new User()
   const defaultAttrs = {
-    firstName: 'Harry',
-    lastName: 'Potter',
-    isCool: true,
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    isCool: faker.random.boolean(),
   }
   return Object.assign(user, { ...defaultAttrs, ...attrs })
 }
