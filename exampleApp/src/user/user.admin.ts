66bc40f499099631cf1b24d1e816cfbaf132bf2f
Duclot William <william.duclot@gmail.com>
2019-10-04 18:07:38 +0100

    User admin: remove description from searchable fields

--- exampleApp/src/user/user.admin.ts
+++ exampleApp/src/user/user.admin.ts
@@ -4,7 +4,7 @@ import { User } from './user.entity'
 export class UserAdmin extends AdminEntity {
   entity = User
   listDisplay = ['id', 'firstName', 'lastName', 'email', 'createdDate']
-  searchFields = ['firstName', 'lastName', 'email', 'description']
+  searchFields = ['firstName', 'lastName', 'email']
 
   widgets = {
     password: PasswordWidget,
