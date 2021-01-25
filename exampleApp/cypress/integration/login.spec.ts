5cf43766f029f94456c0b5c6bec64b2555dab4d8
lucboussant-roux <luc.boussantr@gmail.com>
2021-01-25 18:28:48 +0100

    feat: add a small E2E test

---
+++ exampleApp/cypress/integration/login.spec.ts
@@ -0,0 +1,12 @@
+/// <reference types="Cypress" />
+
+context('Login', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:8000/admin')
+    cy.server()
+  })
+
+  it('should open login page', () => {
+    cy.get('[data-cy=login-info]').should('be.visible')
+  })
+})
